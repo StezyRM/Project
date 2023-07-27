0a5defb5f6c924b8f8fadf8b520bc49feb1554d1
StezyRM <136678129+StezyRM@users.noreply.github.com>
2023-07-27 22:07:14 +0700

    second commit

--- app_advertisment/models.py
+++ app_advertisment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField('Заголовок', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте , если торг уместен')
+    created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
+    update_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+
+    def __str__(self):
+        return
+
+    class Meta:
+        db_table = 'advertisements'
